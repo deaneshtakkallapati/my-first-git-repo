75c0ab54bb3df62b0043db231b4381f0b1a5cf9f
deaneshtakkallapati <60215580+deaneshtakkallapati@users.noreply.github.com>
2024-04-17 17:00:05 +0530

    Update DataFrameExamples.py

--- DataFrameExamples.py
+++ DataFrameExamples.py
@@ -14,22 +14,16 @@ df = pd.read_csv(os.path.join(os.path.dirname(__file__), "A.csv"))
 
 print(df)
 
-# print("----------------------------")
-
-# print(df.loc[(df.A == 'A') & (df.B == 98)])
-
-# print(df.loc[(df.A == 'A') | (df.B >= 98)])
-
- 
-# print(df.loc[df.A.isin(['A'])])
-
- 
+print(df.loc[(df.A == 'A') & (df.B == 98)])
+print("----------")
+print(df.loc[(df.A == 'A') | (df.B >= 98)])
+print("----------")
+print(df.loc[df.A.isin(['A'])])
+print("----------")
 print(df.loc[df.C.notnull()])
 
 
-df = pd.DataFrame([[123,345]])
-
 
 df = pd.DataFrame(np.random.randn(100, 4), columns=list('ABCD'))
-
-df.to_csv(os.path.join(os.path.dirname(__file__), "Random.csv"))
+print("----------")
+print(df.A[0])
