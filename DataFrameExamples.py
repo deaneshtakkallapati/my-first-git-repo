9799159dd6f8ea5133484c774510b64bf970e317
deaneshtakkallapati <60215580+deaneshtakkallapati@users.noreply.github.com>
2024-04-18 02:08:43 +0530

    Update DataFrameExamples.py

--- DataFrameExamples.py
+++ DataFrameExamples.py
@@ -9,6 +9,10 @@ import os
 import pandas as pd
 import numpy as np
 
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "Sample.csv"))
+my_series = pd.Series(np.random.randint(2,7))
+countries_reviewed = df.groupby(['country', 'province']).description.agg([len])
+
 def remean_points(row):
     row.points = row.points - review_points_mean
     return row 
@@ -17,35 +21,31 @@ def my_datafram():
     # Fetch file data using the current dir path
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), "A.csv"))
     
-    print(df)
-    
+    print(df)    
     print(df.loc[(df.A == 'A') & (df.B == 98)])
-    print("----------")
     print(df.loc[(df.A == 'A') | (df.B >= 98)])
-    print("----------")
     print(df.loc[df.A.isin(['A'])])
-    print("----------")
     print(df.loc[df.C.notnull()])
-
-    df = pd.DataFrame(np.random.randn(5, 4), columns=list('ABCD'), index=[0,1,2,3,4])
-    df_1 = pd.DataFrame(np.random.randn(5, 4), columns=list('ABCD'), index=[0,1,2,3,4])
-    
-    df1 = df + df_1
-    
-    df1.to_csv(os.path.join(os.path.dirname(__file__), "Random.csv"))
-    print("----------")
-    print(df1)
-    my_series = pd.Series(np.random.randint(2,7))
-    print("----------")
     print(df.A.unique())
-
-
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "Sample.csv"))
-    # 87, 87, 100, 90, 90, 90
+    # Median
     print(df.points.median())
+    print(df.groupby(['points']).points.count())
+    print(df.groupby(['points']).price.max()) 
+    print(type(df.groupby(['winery']).apply(lambda df: df.title.iloc[0])))
+    print(df.groupby(['country', 'price', 'taster_twitter_handle']).points.agg([len, min, max]))
+    print(df.groupby(['country','province']).apply(lambda df: df.loc[df.points.idxmax()]))
+    print(countries_reviewed)
+    print(countries_reviewed.index)
+    print(countries_reviewed.reset_index())
+    print(countries_reviewed.reset_index().sort_values(by= 'len', ascending=False))
+    print(countries_reviewed.sort_index())
+    print(countries_reviewed.sort_values(by=['country', 'len']))
 
-df = pd.read_csv(os.path.join(os.path.dirname(__file__), "Sample.csv"))
-print(df['points', 'country'])
+
+
+
+
+
 
 
 
